21787cac3cde5fcd62c53f530cba8c7f298db075
himanshu2408 <singh.himanshu2408@gmail.com>
2017-04-23 18:45:05 +0530

    updated api/till mongodb

--- routes/api.js
+++ routes/api.js
@@ -14,19 +14,19 @@ router.get('/', function(req, res, next) {
 
 });
 
-// student api
+// student apis
 router.get('/student', function(req, res, next) {
-    db.companies.find(function (err, companies) {
+    db.students.find(function (err, students) {
         if(err){
             res.send(err);
         }
-        res.json(companies);
+        res.json(students);
     });
 });
 
 // GET particular student
 router.get('/student/:id', function(req, res, next) {
-    db.students.findOne({_id:mongojs.objectId(req.params.id)} ,function (err, student) {
+    db.students.findOne({_id:mongojs.ObjectId(req.params.id)} ,function (err, student) {
         if(err){
             res.send(err);
         }
@@ -57,7 +57,7 @@ router.put('/student/:id', function(req, res, next) {
         });
     }
     else{
-        db.students.update({_id:mongojs.objectId(req.params.id)}, updStudent, {} ,function (err, student) {
+        db.students.update({_id:mongojs.ObjectId(req.params.id)}, updStudent, {} ,function (err, student) {
             if(err){
                 res.send(err);
             }
@@ -88,7 +88,7 @@ router.post('/student', function(req, res, next) {
 
 // Delete a particular student
 router.delete('/student/:id', function(req, res, next) {
-    db.students.remove({_id:mongojs.objectId(req.params.id)} ,function (err, student) {
+    db.students.remove({_id:mongojs.ObjectId(req.params.id)} ,function (err, student) {
         if(err){
             res.send(err);
         }
@@ -96,14 +96,119 @@ router.delete('/student/:id', function(req, res, next) {
     });
 });
 
-// company api
+
+
+// company apis
 router.get('/company', function(req, res, next) {
-   db.students.find(function (err, students) {
-       if(err){
-           res.send(err);
-       }
-       res.json(students);
-   });
+    db.companies.find(function (err, companies) {
+        if(err){
+            res.send(err);
+        }
+        res.json(companies);
+    });
 });
+
+// GET particular company
+router.get('/company/:id', function(req, res, next) {
+    db.companies.findOne({_id:mongojs.ObjectId(req.params.id)} ,function (err, company) {
+        if(err){
+            res.send(err);
+        }
+        res.json(company);
+    });
+});
+
+// update company profile
+router.put('/company/:id', function(req, res, next) {
+    var company = req.body;
+    var updCompany = {};
+    if(company.name){
+        updCompany.name = company.name;
+    }
+    if(company.profile){
+        updCompany.profile = company.profile;
+    }
+    if(company.ctc){
+        updCompany.ctc = company.ctc;
+    }
+    if(company.address){
+        updCompany.address = company.address;
+    }
+    if(!updCompany){
+        res.status(400);
+        res.json({
+            "error": "BAD data"
+        });
+    }
+    else{
+        db.companies.update({_id:mongojs.ObjectId(req.params.id)}, updCompany, {} ,function (err, company) {
+            if(err){
+                res.send(err);
+            }
+            res.json(company);
+        });
+    }
+
+});
+
+// Add new company
+router.post('/company', function(req, res, next) {
+    var company = req.body;
+    if(!company.name || !company.profile || !company.ctc || !company.address){
+        res.status(400);
+        res.json({
+            "error": "BAD data"
+        });
+    }
+    else{
+        db.companies.save(company, function (err, company) {
+            if(err){
+                res.send(err);
+            }
+            res.json(company);
+        });
+    }
+});
+
+// Delete a particular company
+router.delete('/company/:id', function(req, res, next) {
+    db.companies.remove({_id:mongojs.ObjectId(req.params.id)} ,function (err, company) {
+        if(err){
+            res.send(err);
+        }
+        res.json(company);
+    });
+});
+
+
+
+
+// Register student for a company
+router.post('/company/:id/register', function(req, res, next) {
+    var student = req.body;
+    if(!student.name || !student.department || !student.rollno || !student.cgpa){
+        res.status(400);
+        res.json({
+            "error": "BAD data"
+        });
+    }
+    else{
+        db.companies.findOne({_id:mongojs.ObjectId(req.params.id)} ,function (err, company) {
+            if(err){
+                res.send(err);
+            }
+            else{
+
+            }
+        });
+        db.companies.save(company, function (err, company) {
+            if(err){
+                res.send(err);
+            }
+            res.json(company);
+        });
+    }
+});
+
 
 module.exports = router;
